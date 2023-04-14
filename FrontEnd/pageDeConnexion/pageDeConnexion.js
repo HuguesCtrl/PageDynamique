6b06ddad7dd4263d0540e17c396aea20b5b8b2e4
Hugues Collet <hc.pro@outlook.fr>
2023-04-14 11:59:56 +0200

    Formulaire actif

--- FrontEnd/pageDeConnexion/pageDeConnexion.js
+++ FrontEnd/pageDeConnexion/pageDeConnexion.js
@@ -1,4 +1,23 @@
 let form = document.querySelector("#form-login");
-let inputEmail = document.querySelector("#email");
-let inputPassword = document.querySelector("#password");
-console.log(form);
+let inputSubmit = document.querySelector("#sub");
+
+inputSubmit.addEventListener("click", connect);
+
+function connect(e) {
+  e.preventDefault();
+  let inputEmail = document.querySelector("#email");
+  let inputPassword = document.querySelector("#password");
+  if (!inputEmail.value.length == 0 && !inputPassword.value.length == 0) {
+    if (
+      inputEmail.value === "sophie.bluel@test.tld" &&
+      inputPassword.value === "S0phie"
+    ) {
+      window.document.location.href =
+        "http://127.0.0.1:5500/FrontEnd/pageAdministrateur/pageAdmin.html";
+    } else {
+      alert("Mot de passe ou email incorrect");
+    }
+  } else {
+    alert("Veuillez remplir tous les champs");
+  }
+}
